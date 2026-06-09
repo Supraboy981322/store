edf1125d7035b43f3dc82dbba9ecc0fdb60f353b
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-06-09 16:15:56 -0500

    create db if not present

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,13 @@ const types = @import("types.zig");
 const startup = @import("startup.zig");
 const store = @import("store.zig");
 
+var stdout_fi = std.Io.File.stdout();
+const print = std.debug.print;
+const openFile = std.Io.Dir.openFileAbsolute;
+const createFile = std.Io.Dir.createFileAbsolute;
+
 pub fn main(init:std.process.Init) !u8 {
-    var stdout_fi = std.Io.File.stdout();
+
     var stdout_buf:[1024]u8 = undefined;
     var stdout_wr = stdout_fi.writer(init.io, &stdout_buf);
     const stdout = &stdout_wr.interface;
@@ -13,13 +18,17 @@ pub fn main(init:std.process.Init) !u8 {
     var opts:types.Opts = startup.determine_opts(init) catch |e| return hlp.err_out(e);
     defer opts.deinit(init.gpa);
 
-    var file = try std.Io.Dir.openFileAbsolute(init.io, opts.path.?, .{ .mode = .read_write });
+    var file = openFile(init.io, opts.path.?, .{ .mode = .read_write }) catch |e| blk: {
+        if (e == error.FileNotFound)
+            break :blk try createFile(init.io, opts.path.?, .{ .read = true });
+        return hlp.err_out(e);
+    };
     defer file.close(init.io);
 
     switch (opts.act) {
         .get => {
             const val = try store.get_val(init.gpa, init.io, file, opts.key.?) orelse {
-                std.debug.print("key not found: |{s}|\n", .{opts.key.?});
+                print("key not found: |{s}|\n", .{opts.key.?});
                 return 1;
             };
             defer init.gpa.free(val);
